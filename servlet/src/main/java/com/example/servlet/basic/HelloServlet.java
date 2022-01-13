f3f0aeda92ed4af948bfa2e14029f818f3721007
조민규 <inch1209@naver.com>
2022-01-13 23:57:26 +0900

    servlet studying
    infran

---
+++ servlet/src/main/java/com/example/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.example.servlet.basic;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")  // name은 상관 x, 겹치면 안됨
+public class HelloServlet extends HttpServlet {
+    @Override ///hello시 자동 생성
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello" + username);
+
+
+    }
+}
