fd2a5ee29bc9aba8f5ac2041a7e90a3ba6f41982
조민규 <inch1209@naver.com>
2022-01-26 02:24:06 +0900

    JSP studying
    infran

---
+++ servlet/src/test/java/com/example/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.example.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    //싱글톤이라 new없이 getInstance
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMemer = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMemer.getId());
+        assertThat(findMember).isEqualTo(savedMemer);
+    }
+
+    @Test
+    void findAll(){
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 21);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1,member2);
+    }
+
+}
