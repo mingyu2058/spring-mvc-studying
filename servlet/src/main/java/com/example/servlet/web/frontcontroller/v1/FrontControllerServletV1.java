19031589a3840b42c452ad6aa2bc2e118c7ff6e2
조민규 <inch1209@naver.com>
2022-02-04 03:29:18 +0900

    MVC Controller studying
    infran

---
+++ servlet/src/main/java/com/example/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,48 @@
+package com.example.servlet.web.frontcontroller.v1;
+
+import com.example.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import com.example.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import com.example.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@WebServlet(name="frontControllerServletV1",urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+
+    // class가 만들어질 때 key값(위치), controller를 저장
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+
+        //getRequestURI쓰면   http://127.0.0.1:8080/front-controller/v1/save  이 중 front-controller/v1/save  가져옴
+        String requestURI = request.getRequestURI();
+
+        ControllerV1 controller = controllerMap.get(requestURI);
+
+        if(controller==null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        controller.process(request,response);
+
+    }
+}
