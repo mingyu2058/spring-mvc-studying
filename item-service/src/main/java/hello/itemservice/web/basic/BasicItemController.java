4c40f22f9a2733c6fd2c97f87273a1133f0bf719
조민규 <inch1209@naver.com>
2022-03-10 22:23:38 +0900

    edit 추가
    CRUD 완성
    
    infran

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -86,13 +87,47 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item){
         // ModelAttribute 생략 가능, 단순 String 그런 거 말고 직정 생성한 클래스
         // 클래스의 첫글자 소문자가 들어감 so html 적을 때 참고
 
         itemRepository.save(item);
+        // redirect를 쓰면 PostMapping이 아니라 다시 GetMapping으로 돌아감
+        // 그러면 새로고침할 때 계속되는 저장을 막을 수 있음
+        // 이게 PRG  Post/Redirect/Get
         return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV5(Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        //itemId는 치환이 되고 남은 속성인 status는 true로 넘어감
+        // -> /baisc/items/3?status=true
+        return "redirect:/basic/items/{itemId}";
+
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,@ModelAttribute Item item){
+        itemRepository.update(itemId,item);
+        // redirect해서
+        return "redirect:/basic/items/{itemId}";
+
     }
 
 
